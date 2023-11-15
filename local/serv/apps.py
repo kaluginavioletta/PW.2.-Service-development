3fce7c26283a355096c6f52ab5023a688e9101bd
kaluginavioletta <kalugina7bk.ru@yandex.ru>
2023-11-15 21:34:27 +0700

    Creating a Home Page Template

---
+++ local/serv/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ServConfig(AppConfig):
+    verbose_name = 'Доска интерьеров'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'serv'
