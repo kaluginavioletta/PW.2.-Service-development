1d92ffa7073f0dd5adc2f5eeb76acfa73c4bf1bd
kaluginavioletta <129511496+kaluginavioletta@users.noreply.github.com>
2023-12-01 01:13:04 +0700

    Update settings.py

--- local/local/settings.py
+++ local/local/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u5w)6jbt$om6z078o6@@#z46dd^gnq#6+6vzlmf%20d(s$ilpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
